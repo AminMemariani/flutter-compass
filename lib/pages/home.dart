62e6c68c8e7a1cbbec7d803a6578c647a73be3d3
Amin Memariani <amin90memar@gmail.com>
2025-01-06 19:51:02 +0330

    create major and minor ticks

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_compass/flutter_compass.dart';
 
+import 'widgets/compass_painter.dart';
+
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  double _direction = 0.0;
-
   @override
   void initState() {
     super.initState();
     _startCompass();
+//    _fetchPermissionStatus();
   }
 
   @override
@@ -25,92 +26,19 @@ class _HomePageState extends State<HomePage> {
 
   void _startCompass() {
     FlutterCompass.events!.listen((double direction) {
-      setState(() {
-        _direction = direction;
-      });
+      setState(() {});
     } as void Function(CompassEvent event)?);
   }
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            // Background with subtle compass rose
-            Transform.rotate(
-              angle: _direction * (3.14159 / 180), // Convert degrees to radians
-              child: Image.asset(
-                'assets/images/rose.png', // Replace with your compass rose image
-                width: 250,
-                height: 250,
-              ),
-            ),
-            // Compass needle
-            Transform.rotate(
-              angle: _direction * (3.14159 / 180),
-              child: Image.asset(
-                'assets/images/compass_needle.png', // Replace with your compass needle image
-                width: 150,
-                height: 150,
-              ),
-            ),
-            // Cardinal directions (N, E, S, W)
-            Positioned(
-              bottom: 20,
-              child: Text(
-                'N',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Positioned(
-              right: 20,
-              child: Text(
-                'E',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 20,
-              child: Text(
-                'S',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 20,
-              child: Text(
-                'W',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            // Current direction (optional)
-            Positioned(
-              bottom: 80,
-              child: Text(
-                '${_direction.toStringAsFixed(0)}°',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
-        ),
+        body: CustomPaint(
+      size: size,
+      painter: CompassPainterWidget(
+        color: Theme.of(context).colorScheme.primary,
       ),
-    );
+    ));
   }
 }
---
+++ lib/pages/widgets/compass_painter.dart
@@ -0,0 +1,91 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CompassPainterWidget extends CustomPainter {
+  final Color color;
+  final int majorTickerCount;
+  final int minorTickerCount;
+  final CardinalityMap cardinalityMap;
+  CompassPainterWidget({
+    required this.color,
+    this.majorTickerCount = 18,
+    this.minorTickerCount = 90,
+    this.cardinalityMap = const {0: 'N', 90: 'E', 180: 'S', 270: 'W'},
+  });
+
+  late final _majorTicks = _layoutScale(majorTickerCount);
+  late final _minorTicks = _layoutScale(minorTickerCount);
+
+  late final majorScalePaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..color = color
+    ..strokeWidth = 2.0;
+
+  late final minorScalePaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..color = color.withAlpha(178)
+    ..strokeWidth = 1.0;
+
+  late final majorScaleStyle = TextStyle(
+    color: color,
+    fontSize: 12,
+  );
+  late final cardinalityStyle = const TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    fontSize: 20,
+  );
+  @override
+  void paint(Canvas canvas, Size size) {
+    const origin = Offset.zero;
+    final center = size.center(origin);
+    final radius = size.width / 2;
+    final majorTickLength = size.width * 0.08;
+    final minorTickLength = size.width * 0.055;
+
+    canvas.save();
+
+    // Create major ticks
+    for (final angel in _majorTicks) {
+      final tickStart = center +
+          Offset.fromDirection(
+              correctAngel(angel).toRadians(), radius);
+      final tickEnd = center +
+          Offset.fromDirection(
+              correctAngel(angel).toRadians(), radius - majorTickLength);
+      canvas.drawLine(tickStart, tickEnd, majorScalePaint);
+    }
+
+    // Create minor ticks
+    for (final angel in _minorTicks) {
+      final tickStart = center +
+          Offset.fromDirection(correctAngel(angel).toRadians(), radius);
+      final tickEnd = center +
+          Offset.fromDirection(
+              correctAngel(angel).toRadians(), radius - minorTickLength);
+      canvas.drawLine(tickStart, tickEnd, minorScalePaint);
+    }
+
+    canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    throw UnimplementedError();
+  }
+
+  List<double> _layoutScale(int ticks) {
+    final scale = 360 / ticks;
+    return List.generate(ticks, (index) => index * scale);
+  }
+
+  double correctAngel(double angle) => angle - 90;
+}
+
+typedef CardinalityMap = Map<num, String>;
+
+extension on num {
+  double toRadians() => this * pi / 100;
+}
